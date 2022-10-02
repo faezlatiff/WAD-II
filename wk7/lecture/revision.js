c00e9355de3d59a6654961d76b17df963e63e697
faezlatiff <muhammadal.2021@scis.smu.edu.sg>
2022-10-02 13:10:25 +0800

    updates

--- wk7/lecture/revision.js
+++ wk7/lecture/revision.js
@@ -1,25 +1,50 @@
 var url = "https://randomuser.me/api"
-
+​
 function getUser() {
-    // ADD CODE HERE
-    axios.get(url)
-
-    .then(response=>{
+    axios.get(url) // this will make an Async Request
+​
+    .then( response => {
         console.log(response.data)
-        first = response.data.results[0].name.first
-        last = response.data.results[0].name.last.toUpperCase()
-        gender = response.data.results[0].gender
-        email = response.data.results.email
-
-
-
-
+​
+        // read values from response.data
+        var profile =  response.data.results[0]
+​
+        let firstName = profile.name.first
+        let lastName = profile.name.last
+        let email = profile.email
+        let gender = profile.gender
+        let picture = profile.picture.medium
+       
+        // make updates here
+        var infoElem = document.getElementById('info')
+        var pictureElem = document.getElementById('picture')
+        var nameElem = document.getElementById('name')
+        var emailElem = document.getElementById('email')
+        var updateElem = document.getElementById('updateEmail')
+​
+        nameElem.innerText = firstName + " " + lastName.toUpperCase()
+        infoElem.setAttribute('class', gender)
+        
+        pictureElem.src = picture
+        emailElem.innerText = email
+        updateElem.value = email
+​
+        
     })
-
-
-
-
+​
+    .catch( error => {
+        console.log(error.message)
+    })
+​
 }
+​
+function update() {
+    var emailElem = document.getElementById('email')
+    var updateElem = document.getElementById('updateEmail')
+    emailElem.innerText = updateElem.value
+​
+}
+
 function update() {
    // ADD CODE HERE
 
